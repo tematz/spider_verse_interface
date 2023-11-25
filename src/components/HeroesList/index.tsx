4b9a48c7b0f93c2ce722c663fff504ac02010ceb
Matheus Vieira <matvno@gmail.com>
2023-11-25 19:14:04 -0300

    Calling Heroes Details Page on Homepage

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
+import Link from 'next/link'
 
 import HeroPicture from '../HeroPicture'
 
@@ -38,7 +39,9 @@ export default function HeroesList({ heroes }: Iprops) {
             whileTap={{ scale: 0.8 }}
             transition={{ duration: 0.8 }}
           >
-            <HeroPicture hero={hero} />
+            <Link href={`/hero/${hero.id}`}>
+              <HeroPicture hero={hero} />
+            </Link>
           </motion.div>
         ))}
       </motion.section>
