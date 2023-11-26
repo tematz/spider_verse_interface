39695ff4eddd745570e4b2cefed2b752a80718e4
Matheus Vieira <matvno@gmail.com>
2023-11-26 13:38:21 -0300

    Adding the heroes to the carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,9 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
 import HeroDetails from '../HeroDetails'
+import HeroPicture from '../HeroPicture'
 
 import styles from './carousel.module.scss'
 
@@ -10,10 +15,35 @@ interface IProps {
 }
 
 export default function Carousel({ heroes, activeId }: IProps) {
+  const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null)
+  const [activeIndex, setActiveIndex] = useState<number>(
+    heroes.findIndex((hero) => hero.id === activeId)
+  )
+
+  useEffect(() => {
+    const indexInArrayScope =
+      ((activeIndex % heroes.length) + heroes.length) % heroes.length
+    const visibleItems = [...heroes, ...heroes].slice(
+      indexInArrayScope,
+      indexInArrayScope + 3
+    )
+    setVisibleItems(visibleItems)
+  }, [heroes, activeIndex])
+
+  if (!visibleItems) {
+    return null
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.carousel}>
-        <div className={styles.wrapper}>Lista com os heróis</div>
+        <div className={styles.wrapper}>
+          {visibleItems.map((item) => (
+            <div key={item.id} className={styles.hero}>
+              <HeroPicture hero={item} />
+            </div>
+          ))}
+        </div>
       </div>
       <div className={styles.details}>
         <HeroDetails data={heroes[0]} />
