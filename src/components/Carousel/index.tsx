f97f75cd55f52de24eb842fe9ddbd757db35c513
Matheus Vieira <matvno@gmail.com>
2023-11-26 21:06:16 -0300

    Animating the carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 import HeroDetails from '../HeroDetails'
 import HeroPicture from '../HeroPicture'
@@ -8,6 +9,12 @@ import HeroPicture from '../HeroPicture'
 import styles from './carousel.module.scss'
 
 import { IHeroData } from '@/interfaces/heroes'
+
+enum enPosition {
+  FRONT = 0,
+  MIDDLE = 1,
+  BACK = 2,
+}
 
 interface IProps {
   heroes: IHeroData[]
@@ -48,16 +55,52 @@ export default function Carousel({ heroes, activeId }: IProps) {
           className={styles.wrapper}
           onClick={() => handleChangeActiveIndex(1)}
         >
-          {visibleItems.map((item) => (
-            <div key={item.id} className={styles.hero}>
-              <HeroPicture hero={item} />
-            </div>
-          ))}
+          <AnimatePresence mode="popLayout">
+            {visibleItems.map((item, position) => (
+              <motion.div
+                key={item.id}
+                className={styles.hero}
+                initial={{ x: -1500, scale: 0.75 }}
+                animate={{ x: 0, ...getItemStyles(position) }}
+                exit={{ x: 0, opacity: 0, scale: 1, left: '-20%' }}
+                transition={{ duration: 0.8 }}
+              >
+                <HeroPicture hero={item} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
         </div>
       </div>
+
       <div className={styles.details}>
         <HeroDetails data={heroes[0]} />
       </div>
     </div>
   )
+}
+
+const getItemStyles = (position: enPosition) => {
+  if (position === enPosition.FRONT) {
+    return {
+      zIndex: 3,
+      filter: 'blur(10px)',
+      scale: 1.2,
+    }
+  }
+  if (position === enPosition.MIDDLE) {
+    return {
+      zIndex: 2,
+      left: 300,
+      scale: 0.8,
+      top: '-10%',
+    }
+  }
+  return {
+    zIndex: 1,
+    filter: 'blur(10px)',
+    left: 160,
+    top: '-20%',
+    scale: 0.6,
+    opacity: 0.8,
+  }
 }
